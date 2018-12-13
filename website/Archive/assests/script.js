467c24b944a98e30bdb5ed071599dedb13acc886
Manasvi <manasvi@cca.edu>
2018-12-13 14:04:02 -0800

    hmage

---
+++ website/Archive/assests/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+
+// $('#simple_arc').circleType({radius:135});
+
+// $('h1').circleType();
+
+// $('#circle1').CircleType();
+
+  $('#circle1').circleType();
+
+
+// new CircleType(document.getElementById('#circle1'));
+
+});
